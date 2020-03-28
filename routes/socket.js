f803e9904da12cf1fa97adb7505227fcb0fc0543
Nctine <ponlakritkm@gmail.com>
2020-03-28 16:44:17 +0700

    fix socket message

--- routes/socket.js
+++ routes/socket.js
@@ -2,14 +2,17 @@ var express = require('express');
 var router = express.Router();
 var connection = require('../conn');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  // res.sendFile(__dirname + '/index.html');
+router.get('/user/:userId', function(req, res, next) {
+  // res.send(req.params.userId);
 
-  connection.query("SELECT * FROM user_system", function (err, result, fields) {
+  connection.query("SELECT * FROM user_system WHERE USERNAME = '"+req.params.userId+"'", function (err, result, fields) {
     if (err) throw err;
     res.send(result);
   });
+});
+
+router.get('/', function(req, res, next) {
+  res.sendFile('/index.html', { root: 'public' });
 });
 
 module.exports = router;

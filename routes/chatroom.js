a70358abc7fb706abbdcb95a1a696f4660039862
Nctine <ponlakritkm@gmail.com>
2020-04-10 16:24:24 +0700

    version 1.0.0 DEMO

--- routes/chatroom.js
+++ routes/chatroom.js
@@ -15,7 +15,12 @@ router.get('/:chatFrom/:chatTo', function(req, res, next) {
   var params1 = req.params.chatFrom+"_"+req.params.chatTo;
   var params2 = req.params.chatTo+"_"+req.params.chatFrom;
 
-  connection.query("SELECT * FROM chat_room WHERE chat_name = '"+params1+"' OR chat_name = '"+params2+"'", function (err, result, fields) {
+  var query = "SELECT * FROM chat_room r " +
+              "RIGHT JOIN message m ON r.chat_no = m.chat_no " +
+              "WHERE r.chat_name = '" + params1 + "' " +
+              "OR r.chat_name = '" + params2 + "' ";
+
+  connection.query(query, function (err, result, fields) {
     if (err) throw err;
 
     // rows count
@@ -27,17 +32,39 @@ router.get('/:chatFrom/:chatTo', function(req, res, next) {
         connection.query("INSERT INTO message (chat_no) VALUES ('" + result.insertId + "');", function(err, result) {
           if(err) throw err;
           console.log("Insert message completed");
-        });
-      });
 
-      connection.query("SELECT * FROM chat_room WHERE chat_name = '"+params1+"' OR chat_name = '"+params2+"'", function (err, result, fields) {
-        if (err) throw err;
-        res.json(result);
+          connection.query(query, function (err, result, fields) {
+            if (err) throw err;
+            res.json(result);
+          });
+        });
       });
     } else {
       res.json(result);
     }
 
+  });
+});
+
+router.post('/msg', function (req, res) {
+  var query = "UPDATE message SET " +
+              "message_data = '" + JSON.stringify(req.body.message_data) + "', " +
+              "upd_datetime = NOW(), " +
+              "message_recent = '" + req.body.message_recent + "' " +
+              "WHERE message_no = '" + req.body.message_no + "' " +
+              "AND chat_no = '" + req.body.chat_no + "' ";
+
+  connection.query(query, function(err, result) {
+    if (err) throw err;
+    res.json(result);
+  });
+})
+
+// Find and chatroom
+router.get('/msg/:chatNo/:msgNo', function(req, res, next) {
+  connection.query("SELECT message_data FROM message WHERE chat_no = '"+req.params.chatNo+"' AND message_no = '"+req.params.msgNo+"'", function (err, result, fields) {
+    if (err) throw err;
+    res.json(result);
   });
 });
 

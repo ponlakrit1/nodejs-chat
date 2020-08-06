5ca72dc2acca60e0f5c15222c663591c4c461c10
Nctine <ponlakritkm@gmail.com>
2020-08-06 21:40:38 +0700

    add schedule service

--- routes/patient.js
+++ routes/patient.js
@@ -6,7 +6,7 @@ var connection = require('../conn');
 router.get('/record/:pid', function(req, res, next) {
     var query = "SELECT d.name, p.s_text, p.o_text, p.a_text, p.p_text, p.cost, p.note, p.evaluate_score, p.evaluate_note, p.date FROM patient_record p " +
                 "INNER JOIN disease d ON d.id = p.disease_id " +
-                "AND p.patient_id = '" + req.params.pid + "' " +
+                "WHERE p.patient_id = '" + req.params.pid + "' " +
                 "ORDER BY p.date DESC";
   
     connection.query(query, function (err, result, fields) {
@@ -20,13 +20,24 @@ router.get('/therapy/:pid', function(req, res, next) {
     var query = "SELECT tp.frequency, tp.intensity, tp.time, tp.type, t.name, t.link, t.detail, t.frequency as frequency_default FROM therapy_to_patient tp " +
                 "INNER JOIN patient_record p ON p.id = tp.record_id " +
                 "INNER JOIN therapy t ON t.id = tp.therapy_id " +
-                "AND p.patient_id = '" + req.params.pid + "' " +
+                "WHERE p.patient_id = '" + req.params.pid + "' " +
                 "ORDER BY p.date DESC";
   
     connection.query(query, function (err, result, fields) {
       if (err) throw err;
       res.json(result);
     });
+});
+
+// Get schedule
+router.get('/schedule/:recordId', function(req, res, next) {
+  var query = "SELECT * FROM schedule " +
+              "WHERE record_id = '" + req.params.recordId + "' ";
+
+  connection.query(query, function (err, result, fields) {
+    if (err) throw err;
+    res.json(result);
+  });
 });
 
 module.exports = router;

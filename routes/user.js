ec3ed88e1bc95f034a0c65091eb0e7a961032676
Nctine <ponlakritkm@gmail.com>
2020-08-15 13:37:01 +0700

    add doctor for home page service

--- routes/user.js
+++ routes/user.js
@@ -16,6 +16,18 @@ router.get('/patient/:userId', function(req, res, next) {
   });
 });
 
+router.get('/doctor/:doctorId', function(req, res, next) {
+  var query = "SELECT u.title old_title, u.firstname old_firstname, u.lastname old_lastname " +
+              "FROM patient_record pr " +
+              "INNER JOIN `user` u ON u.id = pr.user_id " +
+              "WHERE pr.id = '" + req.params.doctorId + "' ";
+
+  connection.query(query, function (err, result, fields) {
+    if (err) throw err;
+    res.json(result);
+  });
+});
+
 router.get('/', function(req, res, next) {
   connection.query("SELECT * FROM `user` ", function (err, result, fields) {
     if (err) throw err;
